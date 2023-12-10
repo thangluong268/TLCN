2f51ad5088ac620eff420283810588c4ece77b44
LeHaiDang <0509dangle@gmail.com>
2023-12-11 00:36:18 +0700

    .

--- fe/src/components/CardProduct.tsx
+++ fe/src/components/CardProduct.tsx
@@ -8,13 +8,13 @@ function CardProduct(props: any) {
   };
   return (
     <div
-      className="bg-white rounded-md border-2 border-gray-200 mx-2 cursor-pointer hover:shadow-xl"
+      className="bg-white rounded-md border-2 border-gray-200 mx-2 cursor-pointer hover:shadow-xl "
       onClick={(e) => DetailProduct()}
     >
       <img
         src={data.avatar[0]}
         alt=""
-        className="rounded-t-md inset-0 w-full h-full object-cover"
+        className="rounded-t-md inset-0 w-full h-[214px] object-cover"
       />
       <div className="px-2  mt-2">
         <div className="text-ellipsis line-clamp-3 h-[64px] mb-4 text-sm">
