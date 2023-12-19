6c1d98e947fbbac22e57a595062f14301822c38c
LeHaiDang <0509dangle@gmail.com>
2023-12-19 09:40:46 +0700

    .

--- src/app/protectRoute.tsx
+++ src/app/protectRoute.tsx
@@ -1,12 +1,21 @@
-"use client";
 import { redirect } from "next/navigation";
-import React from "react";
 
-export default function protectedPage() {
-  React.useEffect(() => {
+export default function ProtectedPage() {
+  // Check if localStorage is available (e.g., when running on the client side)
+  if (typeof window !== "undefined") {
     const user = JSON.parse(localStorage.getItem("user") || "{}");
-    if (user.role == "Admin") {
+
+    if (user.role === "Admin") {
+      // Redirect to the "/admin" page
       return redirect("/admin");
     }
-  }, []);
+  }
+
+  // If the user is not an admin or if localStorage is not available, you can render the protected content here
+  return (
+    <div>
+      <h1>Protected Page</h1>
+      {/* Add your protected content here */}
+    </div>
+  );
 }
